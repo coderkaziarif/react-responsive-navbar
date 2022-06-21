b95d8a82abf51933470a7528979da8a1985a9492
Kazi Arif <coderkaziarif@gmail.com>
2022-06-22 03:30:34 +0600

    React Responsive Navbar with Toggler

---
+++ src/components/data/navbars.js
@@ -0,0 +1,11 @@
+const topNav = [
+   {id:1, label: "Home", href: "#"},
+   {id:2, label: "About", href: "#"},
+   {id:3, label: "Course", href: "#"},
+   {id:4, label: "Blog", href: "#"},
+   {id:5, label: "Contact", href: "#"},
+];
+
+export const getTopNav = ()=> {
+   return topNav;
+}
